95cfe951342e7e6e197686a930c7d3d9c7a00e29
modemlooper <modemlooper@gmail.com>
2014-12-07 10:21:14 -0800

    cleaning..

--- endpoints/bp-api-activity.php
+++ endpoints/bp-api-activity.php
@@ -29,27 +29,32 @@ class BP_API_Activity {
 		$args = $filter;
 
 		if ( bp_has_activities( $args ) ) {
+
 			while ( bp_activities() ) {
+
 				bp_the_activity();
+
 				$activity = array(
-					'avatar'    		=> bp_core_fetch_avatar( array( 'html' => false, 'item_id' => bp_get_activity_id() ) ),
-					'action'    		=> bp_get_activity_action(),
-					'content'    		=> bp_get_activity_content_body(),
-					'activity_id'  		=> bp_get_activity_id(),
+					'avatar'	 		=> bp_core_fetch_avatar( array( 'html' => false, 'item_id' => bp_get_activity_id() ) ),
+					'action'	 		=> bp_get_activity_action(),
+					'content'	  		=> bp_get_activity_content_body(),
+					'activity_id'		=> bp_get_activity_id(),
 					'activity_username' => bp_core_get_username( bp_get_activity_user_id() ),
-					'user_id'   		=> bp_get_activity_user_id(),
+					'user_id'	 		=> bp_get_activity_user_id(),
 					'comment_count'  	=> bp_activity_get_comment_count(),
-					'can_comment'   	=> bp_activity_can_comment(),
-					'can_favorite'   	=> bp_activity_can_favorite(),
-					'is_favorite'   	=> bp_get_activity_is_favorite(),
+					'can_comment'	 	=> bp_activity_can_comment(),
+					'can_favorite'	  	=> bp_activity_can_favorite(),
+					'is_favorite'	 	=> bp_get_activity_is_favorite(),
 					'can_delete'  		=> bp_activity_user_can_delete()
 				);
-				$activities[] =  $activity;
-				$data = array(
-					'activity' => $activities,
-					'more_activity' => bp_activity_has_more_items()
-				);
+				$activities[] =	 $activity;
+
 			}
+			
+			$data = array(
+				'activity' => $activities,
+				'more_activity' => bp_activity_has_more_items()
+			);
 
 		} else {
 			return new WP_Error( 'bp_json_activity', __( 'No Activity Found.' ), array( 'status' => 200 ) );
@@ -62,34 +67,19 @@ class BP_API_Activity {
 		return $response;
 
 	}
-	
-	/** 
-	 * Function to allow adding of activity within BuddyPress
-	 * 
-	 * @access public 
-	 */ 
+
 	public function add_activity() {
 
 		//add activity code here
 
 	}
-	
-	/** 
-	 * Function to allow editing of activity within BuddyPress
-	 * 
-	 * @access public 
-	 */ 
+
 	public function edit_activity() {
 
 		//edit activity code here
 
 	}
-	
-	/** 
-	 * Function to allow removal of activity within BuddyPress
-	 * 
-	 * @access public 
-	 */ 
+
 	public function remove_activity() {
 
 		//remove activity code here
